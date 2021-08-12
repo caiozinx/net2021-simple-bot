0a738e6a7636b6c524a6ff123546677f739b5557
rm338560 <rm338560@fiap.com.br>
2021-08-12 20:21:17 -0300

    Mock

---
+++ SimpleBotCore/Repositories/AskMock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBotCore.Repositories
+{
+    public class AskMock : IAskRepository
+    {
+        Dictionary<string, string> _ask = new Dictionary<string, string>();
+
+        public void StoreAsk(string ask)
+        {
+            _ask["ask"] = ask;
+        }
+
+    }
+}
--- SimpleBotCore/Startup.cs
+++ SimpleBotCore/Startup.cs
@@ -28,9 +28,10 @@ namespace SimpleBotCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var flagDatabase = Configuration["DatabaseFlag"];
-            
-            if(flagDatabase.Equals("M"))
+            //var flagDatabase = Configuration["DatabaseFlag"];
+            var flagDatabase = "";
+
+            if (flagDatabase.Equals("M"))
             {
                 string connectionString = Configuration["MongoDB:ConnectionString"];
                 MongoClient client = new MongoClient(connectionString);
@@ -43,6 +44,7 @@ namespace SimpleBotCore
             else
             {
                 //mockinit
+                services.AddSingleton<IAskRepository>(new AskMock());
             }
 
             services.AddSingleton<IUserProfileRepository>(new UserProfileMockRepository());
